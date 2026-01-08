58d5e6cc6f0411191576de48f6230cfb42144ccb
yashjawale <yash25.j@gmail.com>
2026-01-09 00:39:15 +0530

    enable update time & author

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,6 +45,8 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/yashjawale/wiki/tree/main/",
+          showLastUpdateTime: true,
+          showLastUpdateAuthor: true,
         },
         blog: false,
         theme: {
